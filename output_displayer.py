01629ea8b601b34e4fc9c45a5953c7136a1b008d
abdikaalbiyan <abdikaalbiyan@gmail.com>
2021-08-07 21:32:19 +0700

    Add files via upload

---
+++ output_displayer.py
@@ -0,0 +1,23 @@
+from scanner import BasicScanner
+from parser import BasicParser
+from executor import BasicExecute
+
+
+
+if __name__ == '__main__':
+	scanner = BasicScanner()
+	parser  = BasicParser()
+	print('Compiler Kita')
+	env = {}
+	
+	while True:
+		
+		try:
+			text = input('Compiler Kita > ')
+		
+		except EOFError:
+			break
+		
+		if text:
+			tree = parser.parse(scanner.tokenize(text))
+			BasicExecute(tree, env)
